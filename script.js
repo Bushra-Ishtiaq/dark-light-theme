4c29cc2ffaef399c6b782c5e0d36cfc03170d748
Bushra Ishtiaq <62174610+Bushra-Ishtiaq@users.noreply.github.com>
2020-07-23 10:25:45 -0700

    uploaded a file

---
+++ script.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    // document.getElementsByTagName("p").style.color = "black";
+    // document.getElementsByTagName("body").style.color = "white";
+
+
+}
+
+//  document.querySelector('section').style.color = pink;
+    // element.classList.toggle("dark-mode");
+    // var x = document.getElementsByTagName("p");
+    // document.getElementsByTagName("p").style.color = "black";
+
+
+    
+    // document.getElementsByTagName("p").style.color = black;
+    // let text = document.querySelector('p');
+    // text.classList.toggle("dark-color");
+ 
